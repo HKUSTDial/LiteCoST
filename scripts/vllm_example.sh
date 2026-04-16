94ab94e954a49d3824d34db1e196dad021c9d5ce
Simon <simonliang484@gmail.com>
2026-04-16 15:25:57 +0800

    111

---
+++ scripts/vllm_example.sh
@@ -0,0 +1,33 @@
+# For Qwen2, you can enable the long-context capabilities by following these steps.
+# modify the config.json file by including the below snippet:
+# access token: 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
+# """
+# {
+#         "architectures": [
+#             "Qwen2ForCausalLM"
+#         ],
+#         // ...
+#         "vocab_size": 152064,
+
+#         // adding the following snippets
+#         "rope_scaling": {
+#             "factor": 4.0,
+#             "original_max_position_embeddings": 32768,
+#             "type": "yarn"
+#         }
+#     }
+# """
+# For details, refer to https://huggingface.co/Qwen/Qwen2-72B-Instruct.
+
+
+
+# export NCCL_DEBUG=INFO
+export CUDA_VISIBLE_DEVICES=0,1
+
+python -m vllm.entrypoints.openai.api_server \
+--port 8088 \
+--served-model-name llama3.2-3b-ins-grpo \
+--gpu_memory_utilization 0.9 \
+--tensor-parallel-size 2 \
+--model "verl/merged/llama3.2-3b-ins" \
+--trust-remote-code 
